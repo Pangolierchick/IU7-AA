cb9ed7b34c031516ddf7f0418c1f05bcd207a749
Pangolierchick <dontgofun@gmail.com>
2021-10-10 03:54:14 +0300

    add optimized vinograd

--- lab_02/src/matrix.rs
+++ lab_02/src/matrix.rs
@@ -6,7 +6,7 @@ pub struct Matrix<T : Default> {
     pub data : Vec<T>
 }
 
-impl<T : Default + num_traits::Num + ops::AddAssign + Copy + ops::SubAssign> Matrix<T> {
+impl<T : Default + num_traits::Num + ops::AddAssign + Copy + ops::Neg + ops::SubAssign + ops::Neg<Output = T>> Matrix<T> {
     pub fn new(r : usize, c : usize) -> Self {
         Self { rows : r, col : c, data: Vec::with_capacity(r * c) }
     }
@@ -101,9 +101,59 @@ impl<T : Default + num_traits::Num + ops::AddAssign + Copy + ops::SubAssign> Mat
 
         Ok(out)
     }
+
+    pub fn vinograd_opt_mult(m1 : & Matrix<T>, m2 : & Matrix<T>) -> Result<Matrix<T>, &'static str> {
+        if m1.col != m2.rows {
+            return Err("The number of columns must be equal to number of rows");
+        }
+
+        let mut out : Matrix<T> = Matrix::new_zero(m1.rows, m2.col);
+
+        let mut row_factor : Vec<T> = vec![Default::default(); m1.rows];
+        let mut col_factor : Vec<T> = vec![Default::default(); m2.col];
+
+        let m1_col_mod = m1.col % 2;
+        let m2_row_mod = m2.rows % 2;
+
+        for i in 0..m1.rows {
+            for j in (0..m1.col - m1_col_mod).step_by(2) {
+                row_factor[i] += m1[[i, j]] * m1[[i, j + 1]]
+            }
+        }
+
+        for i in 0..m2.col {
+            for j in (0..m2.rows - m2_row_mod).step_by(2) {
+                col_factor[i] += m2[[j, i]] * m2[[j + 1, i]]
+            }
+        }
+
+        for i in 0..m1.rows {
+            for j in 0..m2.col {
+                let mut buf : T = -(row_factor[i] + col_factor[j]);
+
+                for k in (0..m1.col - m1_col_mod).step_by(2) {
+                    buf += (m1[[i, k + 1]] + m2[[k, j]]) * (m1[[i, k]] + m2[[k + 1, j]]);
+                }
+
+                out[[i, j]] = buf;
+            }
+        }
+
+        if m1.col % 2 > 0 {
+            let m1c = m1.col - 1;
+            for i in 0..m1.rows {
+                for j in 0..m2.col {
+                    out[[i, j]] += m1[[i, m1c]] * m2[[m1c, j]];
+                }
+            }
+        }
+
+        Ok(out)
+    }
 }
 
-impl<T: fmt::Display + Default + num_traits::Num + ops::AddAssign + Copy + ops::SubAssign> fmt::Display for Matrix<T> {
+impl<T> fmt::Display for Matrix<T> where 
+T: fmt::Display + Default + num_traits::Num + ops::AddAssign + Copy + ops::Neg + ops::SubAssign + ops::Neg<Output = T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut s = String::new();
         for i in 0..self.rows {
